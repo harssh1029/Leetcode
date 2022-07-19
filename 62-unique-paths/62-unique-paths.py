d43ad05039197b390ffe0774f14b018f106de96a
Harsh Upadhyay <63773727+harssh1029@users.noreply.github.com>
2022-07-19 23:12:56 +0530

    Time: 59 ms (24.32%), Space: 13.9 MB (73.29%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -2,34 +2,34 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         
         
-        def getans(m, n, dp):
+        def getans(m, n,dp):
+            
             if m==0 and n==0:
                 return 1
+            
             if m<0 or n<0:
                 return 0
+            
             if dp[m][n]!=-1:
                 return dp[m][n]
             
             up=getans(m-1, n, dp)
             left=getans(m, n-1, dp)
-            dp[m][n]=up+left
+            dp[m][n]=left+up
+            
             return dp[m][n]
-        
-        
-        
+            
+            
+            
         
         
         dp=[]
-        for i in range(m+1):
+        for i in range(m):
             x=[]
-            for j in range(n+1):
+            for j in range(n):
                 x.append(-1)
             dp.append(x)
             
-        if m==1 and n==1:
-            return 1
-                
-        getans(m-1, n-1, dp)
-        # print(dp)
-        return dp[m-1][n-1]
+        
+        return getans(m-1, n-1, dp)
         
