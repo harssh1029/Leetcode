df268310f7eb6b968266b2e248f30d26931fad65
Harsh Upadhyay <63773727+harssh1029@users.noreply.github.com>
2022-07-26 00:28:41 +0530

    Time: 748 ms (64.55%), Space: 71.1 MB (48.00%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.py
+++ 115-distinct-subsequences/115-distinct-subsequences.py
@@ -3,24 +3,20 @@ class Solution:
         
         
         
-        def getans(n, m, s, t, dp):
+#         def getans(n, m, s, t, dp):
             
-            if m<0:
-                return 1
+#             if m<0:
+#                 return 1
             
-            if n<0:
-                return 0
+#             if n<0:
+#                 return 0
             
-            if dp[n][m]!=-1:
-                return dp[n][m]
+#             if dp[n][m]!=-1:
+#                 return dp[n][m]
             
-            if s[n]==t[m]:
-                dp[n][m] = getans(n-1, m-1, s, t, dp) + getans(n-1, m, s, t, dp)
-                return dp[n][m]
             
-            dp[n][m] = getans(n-1, m, s, t, dp)
             
-            return dp[n][m]
+#             return dp[n][m]
         
         
         
@@ -32,11 +28,22 @@ class Solution:
         
         n=len(s)
         m=len(t)
-        for i in range(n):
+        for i in range(n+1):
             x=[]
-            for j in range(m):
-                x.append(-1)
+            for j in range(m+1):
+                x.append(0)
             dp.append(x)
-        return getans(n-1, m-1, s, t, dp)
+            
+        for i in range(n+1):
+            dp[i][0]=1
+            
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                if s[i-1]==t[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j]
+        
+        return dp[n][m]
         
         
