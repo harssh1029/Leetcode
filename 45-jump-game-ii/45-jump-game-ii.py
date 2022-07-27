bde244bad314eb37411161092afdc8271d9c6aca
Harsh Upadhyay <63773727+harssh1029@users.noreply.github.com>
2022-07-28 03:29:59 +0530

    Time: 10268 ms (15.43%), Space: 14.8 MB (97.30%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -3,25 +3,38 @@ class Solution:
         
         
         
-        def getans(idx, nums, n, dp):
-            if idx==n:
-                return 0
+#         def getans(idx, nums, n, dp):
+#             if idx==n:
+#                 return 0
             
-            if idx>n:
-                return float("inf")
+#             if idx>n:
+#                 return float("inf")
             
-            if dp[idx]!=-1:
-                return dp[idx]
+#             if dp[idx]!=-1:
+#                 return dp[idx]
             
-            steps=float("inf")
-            for i in range(1, nums[idx]+1):
-                steps = min(steps, 1+getans(idx+i, nums, n, dp))
+#             steps=float("inf")
+#             for i in range(1, nums[idx]+1):
+#                 steps = min(steps, 1+getans(idx+i, nums, n, dp))
                 
-            dp[idx]=steps
-            return dp[idx]
+#             dp[idx]=steps
+#             return dp[idx]
         
         
+#         n=len(nums)
+#         dp=[-1]*len(nums)
+#         return getans(0, nums, n-1, dp)
+
         n=len(nums)
-        dp=[-1]*len(nums)
-        return getans(0, nums, n-1, dp)
+        dp=[0]*n
+        
+        for i in range(n-2, -1, -1):
+            temp=float("inf")
+            for j in range(1, nums[i]+1):
+                if i+j < n:
+                    temp = min(temp, 1+dp[i+j])
+                
+            dp[i]=temp
+                
+        return dp[0]
         
