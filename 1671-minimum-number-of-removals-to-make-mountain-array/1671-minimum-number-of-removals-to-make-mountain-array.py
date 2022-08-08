8f4270642316b0bcd11d442bcee9b14e14435660
Harsh Upadhyay <63773727+harssh1029@users.noreply.github.com>
2022-08-09 02:21:07 +0530

    Time: 4205 ms (42.28%), Space: 14 MB (88.62%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.py
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.py
@@ -4,19 +4,22 @@ class Solution:
         dp1=[1]*n
         dp2=[1]*n
         ans=0
-        for i in range(n):
-            for j in range(i):
-                if nums[i]>nums[j] and dp1[i]<1+dp1[j]:
-                    dp1[i]=dp1[j]+1
-                    
+        for i in range(0, n):
+            for j in range(0, i):
+                if nums[i]>nums[j] and 1+dp1[j]>dp1[i]:
+                    dp1[i]=1+dp1[j]
                     
         for i in range(n-1, -1, -1):
             for j in range(n-1, i, -1):
-                if nums[i]>nums[j] and dp2[i]<1+dp2[j]:
-                    dp2[i]=dp2[j]+1
-                    
+                if nums[i]>nums[j] and 1+dp2[j]>dp2[i]:
+                    dp2[i]=1+dp2[j]
+        
             if dp1[i]>1 and dp2[i]>1:
                 ans=max(ans, dp1[i]+dp2[i]-1)
+        
+        
+#         for i in range(n):
+#             ans=max(ans, dp1[i]+dp2[i]-1)
         
         return n-ans
                 
